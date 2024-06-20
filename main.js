4310e2d62bdaf424abf3edabd4134040aa0100c7
millan figueroa <millan.fig@gmail.com>
2024-06-19 20:28:57 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const contents = document.getElementById('contents');
+contents.parentNode.removeChild(contents);
+
+//create new div
+//attach image link to new img element
+//attach div with img inside to document
+//create an H1 with content
+//attach to div
